2a94899832f6517e22acf0846ce2d277534a956b
Ahmad <myvirtualrabbit15@gmail.com>
2021-07-22 21:25:13 +0600

    new

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section raw nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        fáilte <br /> Meine Portfolio
+      </SectionTitle>
+      <SectionText>Let's see what We build next...</SectionText>
+    </LeftSection>
+    <button
+      onClick={(e) =>
+        (window.location = 'https://developer.mozilla.org/en-US/')
+      }
+    >
+      Learn More
+    </button>
+  </Section>
 );
 
 export default Hero;
